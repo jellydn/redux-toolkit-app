7b50d29c3601619ae4b1530283a53184a7de8d50
Huynh Duc Dung <dung@aircarbon.co>
2021-06-19 23:52:32 +0800

    :recycle: npx @next/codemod cra-to-next

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
+// import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function NextIndexWrapper() {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
