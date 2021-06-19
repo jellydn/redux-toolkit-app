02d2f51b3832beb191bcad4128aaba64e66444ce
Huynh Duc Dung <dung@aircarbon.co>
2021-06-20 01:32:36 +0800

    :lipstick: change app title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Redux App</title>
+        <title>React Redux App (NextJS v11)</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
