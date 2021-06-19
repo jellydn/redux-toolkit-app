b7116586fa9b0796ef988ffb9e388bf180df9558
Huynh Duc Dung <dung@aircarbon.co>
2021-06-20 01:28:17 +0800

    :fire: remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-// import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import { store } from "./app/store";
 
 export default function NextIndexWrapper() {
   return (
@@ -15,8 +13,3 @@ export default function NextIndexWrapper() {
     </React.StrictMode>
   );
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
