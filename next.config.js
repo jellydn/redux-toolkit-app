305f44d06b8a5474d49acbdfb4e0185f581f271f
Huynh Duc Dung <dung@aircarbon.co>
2021-06-20 00:51:19 +0800

    :sparkles: add jest config, linter and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    PUBLIC_URL: ''
+    PUBLIC_URL: "",
   },
   experimental: {
     craCompat: true,
@@ -8,6 +8,6 @@ module.exports = {
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
-    disableStaticImages: true
-  }  
-}
+    disableStaticImages: true,
+  },
+};
